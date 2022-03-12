1425722edc8c4fa2ed46aaf5b49c713dc09f0207
Markus Bergkvist <markus@familjenbergkvist.net>
2022-03-12 13:00:43 +0100

    Add aws_lambda_events_macro
    It does not build yet due to different structure of AttributeValue.

--- src/macros/mod.rs
+++ src/macros/mod.rs
@@ -1,7 +1,9 @@
+mod aws_lambda_events;
 mod aws_sdk;
 mod rusoto;
 mod rusoto_streams;
 
+pub(crate) use aws_lambda_events::aws_lambda_events_macro;
 pub(crate) use aws_sdk::aws_sdk_macro;
 pub(crate) use rusoto::rusoto_macro;
 pub(crate) use rusoto_streams::rusoto_streams_macro;
